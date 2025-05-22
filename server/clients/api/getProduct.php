c96a42674a548c63ed3a4ac774f8e3cf34ea25ac
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-05-22 14:45:30 +0100

    fix the auth error that occurs when fetch product

--- server/clients/api/getProduct.php
+++ server/clients/api/getProduct.php
@@ -2,18 +2,13 @@
 include('../../connection.php');
 require_once '../../config.php'; // Adjust path if needed
 
-
-$projectRoot = dirname(__DIR__,3);
+$projectRoot = dirname(__DIR__, 3);
 loadEnv($projectRoot . '/.env');
 
+$storefrontToken = getenv('STOREFRONT_TOKEN');
+$storefrontEndpoint = getenv('STOREFRONT_ENDPOINT');
 
-$accessToken = getenv('ACCESS_TOKEN');
-$secretKey = getenv('SECRET_KEY');
-$shopUrl = getenv('SHOP_URL');
-
-echo $accessToken;
-
-// Shopify GraphQL query
+// Storefront GraphQL Query
 $query = <<<GQL
 {
   products(first: 100) {
@@ -21,26 +16,16 @@ $query = <<<GQL
       node {
         id
         title
-        productType         # Category
+        productType
         tags
         options {
           name
           values
         }
-        collections(first: 1) {
-          edges {
-            node {
-              title
-              metafield(namespace: "custom", key: "target_gender") {
-                value
-              }
-            }
-          }
-        }
         images(first: 1) {
           edges {
             node {
-              src
+              url
             }
           }
         }
@@ -48,7 +33,10 @@ $query = <<<GQL
           edges {
             node {
               id
-              price
+              price {
+                amount
+                currencyCode
+              }
               selectedOptions {
                 name
                 value
@@ -63,12 +51,12 @@ $query = <<<GQL
 GQL;
 
 // Send the GraphQL request
-$ch = curl_init("https://$shopUrl/admin/api/2025-01/graphql.json");
+$ch = curl_init($storefrontEndpoint);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
   'Content-Type: application/json',
-  "X-Shopify-Access-Token: $accessToken",
+  "X-Shopify-Storefront-Access-Token: $storefrontToken",
 ]);
 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['query' => $query]));
 $response = curl_exec($ch);
@@ -79,7 +67,6 @@ if (curl_errno($ch)) {
 }
 curl_close($ch);
 
-// Decode the JSON response
 $responseData = json_decode($response, true);
 $products = $responseData['data']['products']['edges'] ?? [];
 
@@ -91,29 +78,24 @@ foreach ($products as $product) {
   preg_match('/(\d+)$/', $variantGID, $matches);
   $variantID = $matches[1] ?? null;
 
-  // Extract options (Size, Color, etc.)
+  $price = $node['variants']['edges'][0]['node']['price']['amount'] ?? '';
+
   $options = [];
   foreach ($node['options'] as $option) {
     $options[$option['name']] = $option['values'];
   }
 
-  // Extract collection & target gender
-  $collection = $node['collections']['edges'][0]['node'] ?? null;
-  $category = $collection['title'] ?? $node['productType'] ?? '';
-  $targetGender = $collection['metafield']['value'] ?? '';
-
   $cleanedProducts[] = [
     'title' => $node['title'],
-    'image' => $node['images']['edges'][0]['node']['src'] ?? '',
-    'price' => $node['variants']['edges'][0]['node']['price'] ?? '',
+    'image' => $node['images']['edges'][0]['node']['url'] ?? '',
+    'price' => $price,
     'variant_id' => $variantID,
-    'category' => $category,
+    'category' => $node['productType'],
     'tags' => $node['tags'] ?? [],
     'options' => $options,
-    'target_gender' => $targetGender
+    'target_gender' => '' // Storefront API doesn't expose metafields directly for collections
   ];
 }
 
-// Output JSON
 header('Content-Type: application/json');
 echo json_encode(['products' => $cleanedProducts]);
